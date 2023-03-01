04626a21e7303f0552dfff02b78fd9d1ace85840
MarcusJul <Lancecheung960425@hotmail.com>
2023-03-02 01:27:43 +0800

    Update This is a binary search Tree.py

--- IPK/Trees/This is a binary search Tree.py
+++ IPK/Trees/This is a binary search Tree.py
@@ -1,18 +1,37 @@
 def checkBST(root):
     d = {}
-    def filld(data):
-        if data in d:return False
-        else: d[data] = 1
+    def checkB(node, cmax, cmin):# cmax is max of (left) child, cmin is min of (right) child
+        # Current node
+        if node==None:
+            return True, cmax, cmin
+        else:
+            if node.data in d:
+                d[node.data] += 1
+            else:
+                d[node.data] = 1
             
-    def CheckB(node):
-        if node==None:return False
-        if not filld(node.data): return False
-        if node.left!=None: resl = CheckB(node.left) 
-        else: resl = True
-        if node.right!=None: resr = CheckB(node.right) 
-        else: resr = True
-        return all([resl, resr])
-    return CheckB(root)
+            
+        # node data larger than max of left
+        if node.left!=None:
+            resl, cmaxl, cminl = checkB(node.left, cmax, cmin)
+        else:
+            resl, cmaxl, cminl = True, cmax, cmin
+        # node data less than min of right
+        if node.right!=None:
+            resr, cmaxr, cminr = checkB(node.right, cmax, cmin)
+        else:
+            resr, cmaxr, cminr = True, cmax, cmin
+        
+        if cmaxl>node.data or cminr<node.data:
+            res = False
+        else:
+            if all([resl, resr]):
+                res = True
+        
+        return res, max([node.data, cmaxr, cmaxl]), min([node.data, cminl, cminr])
+    for k,v in d.items():
+        if v>1:return False
+    return checkB(root, -1, 10001)[0]
 
 """
 2
