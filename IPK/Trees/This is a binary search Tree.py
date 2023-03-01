22ec353f5088e6315497484d4a89e75ba745a115
MarcusJul <Lancecheung960425@hotmail.com>
2023-03-02 01:49:59 +0800

    Update This is a binary search Tree.py

--- IPK/Trees/This is a binary search Tree.py
+++ IPK/Trees/This is a binary search Tree.py
@@ -1,3 +1,11 @@
+""" Node is defined as
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+"""
+
 def checkBST(root):
     d = {}
     def checkB(node, cmax, cmin):# cmax is max of (left) child, cmin is min of (right) child
@@ -21,17 +29,20 @@ def checkBST(root):
             resr, cmaxr, cminr = checkB(node.right, cmax, cmin)
         else:
             resr, cmaxr, cminr = True, cmax, cmin
-        
+            
+        res = True
         if cmaxl>node.data or cminr<node.data:
             res = False
-        else:
-            if all([resl, resr]):
-                res = True
         
-        return res, max([node.data, cmaxr, cmaxl]), min([node.data, cminl, cminr])
-    for k,v in d.items():
-        if v>1:return False
-    return checkB(root, -1, 10001)[0]
+        return all([res, resr, resl]), max([node.data, cmaxr, cmaxl]), min([node.data, cminl, cminr])
+ 
+    res = checkB(root, -1, 10001)[0]
+    if sum(d.values())>len(d.keys()):
+        return False
+
+    return res
+        
+    
 
 """
 2
@@ -42,6 +53,11 @@ Yes
 3 5 7 9 11 13 15
 Yes
 
+2
+1 2 2 4 5 6 7
+No
 
-
+4
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 16 18 19 20 21 22 23 24 25 26 27 28 29 30 31
+No
 """
