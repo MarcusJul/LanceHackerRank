d3f09e66a8a0533a9b51fe18ce9ab8e0ca1bbc21
MarcusJul <Lancecheung960425@hotmail.com>
2023-03-04 22:35:09 +0800

    Update Davis staircase.py

--- IPK/Recursion and backtracking/Davis staircase.py
+++ IPK/Recursion and backtracking/Davis staircase.py
@@ -16,3 +16,18 @@ def stepPerms(n):
                 d[n-1] = s1
             return s1+s2+s3
     return countstep(n,d)
+
+def stepPerms(n): # same but shorter
+    # Write your code here
+    d = {1:1, 2:2, 3:4}
+    def countstep(n, d):
+        if n in d:
+            return d[n]
+        else:
+            out = [0,0,0]
+            for i in range(1,4):
+                out[i-1] = countstep(n-i, d) if n>i else 0
+                if n-i not in d:
+                    d[n-i] = out[i-1]
+            return sum(out)
+    return countstep(n,d)
